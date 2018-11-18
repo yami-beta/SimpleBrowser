92dfd969e657a097313b738af1a1945d6ae55071
yami-beta <yami-beta@users.noreply.github.com>
2018-11-18 14:08:22 +0900

    Initial Commit

---
+++ SimpleBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by Takahiro Abe on 2018/11/18.
+//  Copyright © 2018 yami-beta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
