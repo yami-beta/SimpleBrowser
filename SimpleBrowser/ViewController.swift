a7967bb4bf2f5070d22e7e2e8ad1fb9b0b7c982e
yami-beta <yami-beta@users.noreply.github.com>
2018-11-18 15:16:36 +0900

    open関数を実装し起動時に指定のURLを開く

--- SimpleBrowser/ViewController.swift
+++ SimpleBrowser/ViewController.swift
@@ -15,11 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var webView: UIWebView!
     
+    // ホームページのURL。起動時にこのページを開く。
+    let homeUrlString = "http://www.yahoo.co.jp"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // ホームページを開く。
+        open(urlString: homeUrlString)
     }
 
+    // 文字列で指定されたURLをWeb Viewで開く。
+    func open(urlString: String) {
+      let url = URL(string: urlString)
+      let urlRequest = URLRequest(url: url!)
+      webView.loadRequest(urlRequest)
+    }
 
     @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
     }
